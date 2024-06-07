d5e337108da3ccf00d979b78b8d2ea78be4ad1e6
saitoushiori <saitoushiori@localhost>
2024-06-07 21:26:16 +0900

    [First]commit

---
+++ src/apple/app/apple1.java
@@ -0,0 +1,10 @@
+package apple.app;
+
+public class apple1 {
+	public static void main(String args[]) {
+		for (int number = 10; number >= 0; number--) {
+			System.out.println(number);
+		}
+	}
+
+}
