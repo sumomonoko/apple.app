151cf7309d3fbddf96adfe4c8498153c2bd5d6a8
saitoushiori <saitoushiori@localhost>
2024-06-08 16:23:52 +0900

    変数の初期化と更新

--- src/apple/app/banana.java
+++ src/apple/app/banana.java
@@ -1,15 +1,15 @@
 package apple.app;
 
-//クラス部分
+//クラス部分↓↓
 public class banana {
-//	メソッド部分
+//	メソッド部分↓↓
 	public static void main(String[] args) {
 //		処理部分(※文末にセミコロン)
 //		文字列↓↓
 	    System.out.println("Hello Java");
 //	    数値↓↓
 	    System.out.println(17);
-//	    ダブルクォーテーションで囲むと文字列になる
+//	    ※ダブルクォーテーションで囲むと文字列になる
 	    System.out.println("5+3");
 //	    足し算
 	    System.out.println(5+3);
@@ -21,5 +21,35 @@ public class banana {
 	    System.out.println(5/3);
 //	    割り算の余り
 	    System.out.println(5%3);
-	  }
+	    
+	    
+//		変数↓↓   (intは整数、Stringは文字列のデータ型)
+		// int型の変数numberを定義
+	    int number;
+	    // 変数numberに3を代入
+	    number = 3;
+	    // 変数numberを出力
+	    System.out.println(number);
+	    // String型の変数nameを定義
+	    String name;
+	    // 変数nameに"Wanko"を代入
+	    name = "Wanko";
+	    // 変数nameを出力
+	    System.out.println(name);
+	    
+	    // 変数の初期化=変数定義と同時に値を代入すること
+	    int number1 = 3;
+	    System.out.println(number1*3);
+	    
+	    String text = "プログラミングを勉強しよう";
+	    System.out.println("Progateで"+text);
+	    
+	    // 変数の更新(上書き)
+	    number = 11;
+	    name = "Satou";
+	    System.out.println(number);
+	    System.out.println(name);
+	    
+	    
+	}
 }
