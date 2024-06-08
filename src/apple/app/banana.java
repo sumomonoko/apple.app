a6e133a5a9fec6413e393670e50ffa8a1fb027f5
saitoushiori <saitoushiori@localhost>
2024-06-08 16:03:22 +0900

    文字列と数値

---
+++ src/apple/app/banana.java
@@ -0,0 +1,25 @@
+package apple.app;
+
+//クラス部分
+public class banana {
+//	メソッド部分
+	public static void main(String[] args) {
+//		処理部分(※文末にセミコロン)
+//		文字列↓↓
+	    System.out.println("Hello Java");
+//	    数値↓↓
+	    System.out.println(17);
+//	    ダブルクォーテーションで囲むと文字列になる
+	    System.out.println("5+3");
+//	    足し算
+	    System.out.println(5+3);
+//	    引き算
+	    System.out.println(5-3);
+//	    掛け算
+	    System.out.println(5*3);
+//	    割り算
+	    System.out.println(5/3);
+//	    割り算の余り
+	    System.out.println(5%3);
+	  }
+}
